26c404fcde54f615b5a443625919ae46ea4f96c5
vdi-terminal <vdi-terminal@EDU-WSB.local>
2023-05-07 12:36:12 +0200

    update

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\vdi-terminal\PycharmProjects\cw_praca_z_kodem\venv
+set VIRTUAL_ENV=C:\Users\vdi-terminal\Desktop\test-github-workflow\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
